42b41adc83a13da84c170eed2445785e23bf2838
CharlieBludau <charlieb41@hotmail.com>
2026-07-31 18:24:34 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='charlies-cookbook-v1';
+const CACHE='charlies-cookbook-v2';
 const ASSETS=['./','./index.html','./styles.css','./recipes.js','./app.js','./manifest.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
